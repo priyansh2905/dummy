e14f8b8ff134fca6b346cbebd8557fb5ae9e7c22
Priyansh Sharma <sharmapriyansh623@gmail.com>
2024-12-18 18:15:20 +0530

    first commit

---
+++ request-response/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter' , (req,res) => {
+    res.send('Priyansh2905')
+})
+
+app.get('/login' , (req,res) => {
+    res.send('<h1>Please login</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
